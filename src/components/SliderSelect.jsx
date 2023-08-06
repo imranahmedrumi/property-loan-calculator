3c36fb4721f5a1af0d60e74257a787446cc30251
ahmad-imran <imrannobine@gmail.com>
2023-08-06 21:00:46 +0600

    Update

--- src/components/SliderSelect.jsx
+++ src/components/SliderSelect.jsx
@@ -5,10 +5,34 @@ const SliderSelect = () => {
   return (
     <>
       <SliderComponent
+        label="Home Value"
         min={0}
         max={50}
         defaultValue={20}
         step={5}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={3000}
+      />
+      <SliderComponent
+        label="Down Payment"
+        min={0}
+        max={50}
+        defaultValue={20}
+        step={5}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={500}
+      />
+      <SliderComponent
+        label="Loan Amount"
+        min={0}
+        max={50}
+        defaultValue={20}
+        step={5}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={700}
       />
 
     </>
